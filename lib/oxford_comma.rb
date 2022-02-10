1612f9acccf1b1b3044223b73762229454aecea3
cd1412 <chiduong1412@gmail.com>
2022-02-09 23:14:56 -0600

    Completed assignment

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,14 @@
 def oxford_comma(array)
-
+    if array.length == 1
+        return array.join
+    elsif array.length == 2
+        return array.join(" and ")
+    else 
+        array.each.collect do |length|
+            if length == array[-1]
+                array[-1] = "and " << length
+            end
+        end
+        array.join(", ")
+    end
 end
